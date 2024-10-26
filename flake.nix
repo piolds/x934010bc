217170b42d26e2475d5e215e4d63c7e5c5674025
piolds <134967358+piolds@users.noreply.github.com>
2024-10-26 16:43:31 +0100

    Add files via upload
    Add flake-nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Dwm-x-Nix configuration for different systems I have";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+
+    nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
+
+            modules = [
+                home-manager.nixosModules.home-manager {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    users.users.user = {
+                        isNormalUser = true;
+                        imports = [];
+                    };
+                }
+            ];
+        };
+
+        defaultIso = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
+            # XXX: I think this is here in case I want to add packages to nixos iso
+        };
+    };
+  };
+}
